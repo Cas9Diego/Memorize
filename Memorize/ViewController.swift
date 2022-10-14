b52030da6c5b7c199b5a6127319f3dff37eba4d3
Cas9Diego <DaCastro@outlook.com>
2022-10-14 01:55:21 -0500

    Initial Commit

---
+++ Memorize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memorize
+//
+//  Created by Diego Castro on 14/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
